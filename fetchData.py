ee7eb9127c2df560aa32ce3ca8996336eae9ddde
monil-georgian <65424895+monil-georgian@users.noreply.github.com>
2020-07-29 14:15:27 -0400

    Update fetchData.py

--- fetchData.py
+++ fetchData.py
@@ -1,11 +1,7 @@
 import requests
 
-url = "https://superheroapi.com/api/4278325702207782/"
+url = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json"
 
-query = {
-  "character-id": 1
-}
-
-response = requests.request("GET", url, params=query)
+response = requests.request("GET", url)
 
 print(response.text)
